d0f74a45613567d2cfa09aeb616eda7e27dbf5ea
claudio <claudioretuerto@hotmail.com>
2022-09-04 17:07:10 -0300

    inicio

---
+++ main/scrip.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.portada__presentacion', {delay:200});
+ScrollReveal().reveal('.portada__farmacos', {delay:700});
+ScrollReveal().reveal('.portada__patologias', {delay:700});
+ScrollReveal().reveal('.portada__anatomia', {delay:700});
